4f3890985f2a42683b03dc5caec45db1b109a0b3
shiliangL <1462999406@qq.com>
2022-06-20 18:02:29 +0800

    init list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <!--
  * @Author: shiliangL
  * @Date: 2022-06-20 16:20:54
- * @LastEditTime: 2022-06-20 17:55:13
+ * @LastEditTime: 2022-06-20 17:59:35
  * @LastEditors: Do not edit
  * @Description:
 -->
 <template>
   <div class="page">
-    Welcome
     <DraggableElement></DraggableElement>
   </div>
 </template>
