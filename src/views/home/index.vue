3a0bc4cf86cc56a39dea622768f5c2c2f2a33ec5
shiliangwei <1462999406@qq.com>
2022-07-21 15:48:30 +0800

    新组件+++

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: shiliangL
  * @Date: 2022-06-20 16:20:54
- * @LastEditTime: 2022-07-21 15:37:32
+ * @LastEditTime: 2022-07-21 15:47:50
  * @LastEditors: Do not edit
  * @Description:
 -->
@@ -87,7 +87,7 @@ export default {
             // 设置网格中Slide的行数。
             // 当设置row>1时目前还不兼容loop模式（loop: true）。
             // 在Slide数量不足以填满的情况下可能会出现布局不理想（例如4行3列，但是只有10个Slide），建议使用空的Slide将数量补足
-            fill: 'row' // 设置网格的排列方向，默认column为竖向，row为横向
+            fill: 'row' // 设置网格的排列方向，默认 column 为竖向，row为横向
           }
         },
         carouselStyle: {
@@ -101,12 +101,12 @@ export default {
           slidesPerView: 3,
           spaceBetween: 4,
           grid: {
-            rows: 2,
+            rows: 3,
             fill: 'row'
           }
         },
         carouselStyle: {
-          width: 'calc((100% - 4px) / 2)'
+          width: 'calc((100% - 4px) / 3)'
         }
       }
     }
